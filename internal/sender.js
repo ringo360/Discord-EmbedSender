43afb044539d245d5540aeeb10dea265b00db8fb
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-22 23:25:25 +0900

    !

--- internal/sender.js
+++ internal/sender.js
@@ -2,7 +2,13 @@ const ansired = '\x1b[31m';
 const ansigreen = '\x1b[32m';
 
 function send(questions, answers) {
-	
+	console.log("Called!")
+	for (let i = 0; i < questions.length; i++) {
+		if (answers[i] === '') {
+			answers[i] = 'Not entered'
+		}
+		console.log(`${questions[i]} => ${answers[i]}`);
+	  }
 }
 
 module.exports = {
