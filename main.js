322ee911da1254c2f45bd45d3dd427abfa3d5d3d
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-22 22:06:03 +0900

    umm

--- main.js
+++ main.js
@@ -1,43 +1,31 @@
 const readline = require('readline');
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-// const rpcfunc = require('./internal/rpcfunc.js');
-
-console.log("Welcome to DiscordEmbedSender!")
-console.log("")
-rl.question('Press any key to continue...', (ans) => {
-	console.log(`detect: ${ans}`)
-	rl.close
-})
-
-function ask(question)) {
-	rl.question(`${question}\n` + '>> ', (ans) => {
-		console.log(ans)
-		const number = parseInt(ans);
-	
-		if (isNaN(number)) {
-			console.log('Invalid input. Please enter a number(1~5)!')
-		} else if (number >= 1 && number <= 5) {
-			switch (number) {
-				case 1:
-					console.log("1");
-					rpcfunc.create('test');
-					break;
-				/*
-				case 2:
-					console.log("2");
-					rpcfunc.edit('test2')
-					break;
-				*/
-
-				default:
-					console.log(`detect ${number}`);
-			}
-		} else {
-			console.log('Invalid input. Please enter a number(1~5)!')
-		}
-	})
+function modeselect() {
+  console.log("a");
 }
+
+function ask(question) {
+  rl.question(`${question}\n>> `, (ans) => {
+    console.log(`ans: ${ans}`);
+    if (ans === '') { 
+      ask(question);
+    } else {
+      rl.close(); 
+    }
+  });
+}
+
+console.log("Welcome to DiscordEmbedSender!");
+console.log("");
+
+rl.question('Press any key to continue...', (ans) => {
+  console.log(`detect: ${ans}`);
+  rl.close(); 
+});
+
+console.log("loading");
+ask("TEST");
