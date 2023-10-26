f5ba55e10342ed75f653e56bf7397358695b5db2
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-26 22:04:36 +0900

    const

--- internal/sender.js
+++ internal/sender.js
@@ -3,12 +3,15 @@ const ansigreen = '\x1b[32m';
 
 function send(questions, answers) {
 	console.log("Called!")
-	for (let i = 0; i < questions.length; i++) {
-		if (answers[i] === '') {
-			answers[i] = 'Not entered'
-		}
-		console.log(`${questions[i]} => ${answers[i]}`);
-	  }
+	const webhookurl = answers[0]
+	const title = answers[1]
+	const desc = answers[2]
+	const color = answers[3]
+	const author = answers[4]
+	const field = answers[5]
+	const footer = answers[6]
+	let webhookClient = new WebhookClient({ url: webhookurl });
+	console.log("Sending Webhook...")
 }
 
 module.exports = {
