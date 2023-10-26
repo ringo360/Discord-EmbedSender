dec90982799ea6b6eb240cf0428c639cf9e074a7
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-26 22:36:55 +0900

    oopsie

--- internal/sender.js
+++ internal/sender.js
@@ -5,12 +5,11 @@ const { WebhookClient } = require("discord.js");
 function send(questions, answers) {
 	console.log("Called!")
 	const webhookurl = answers[0]
-	const title = answers[1]
-	const desc = answers[2]
-	const color = answers[3]
-	const author = answers[4]
-	const field = answers[5]
-	const footer = answers[6]
+	const title = answers[3]
+	const desc = answers[4]
+	const color = answers[5]
+	const author = answers[6]
+	const footer = answers[7]
 	const webhookClient = new WebhookClient({ url: webhookurl });
 	console.log("Sending Webhook...")
 	webhookClient.send({
