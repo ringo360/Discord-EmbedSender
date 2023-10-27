14bd18f806d4356566427fe3192f3a0add18aa01
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-27 21:36:16 +0900

    bypass undefined

--- internal/sender.js
+++ internal/sender.js
@@ -4,7 +4,10 @@ const { WebhookClient } = require("discord.js");
 
 function send(questions, answers) {
 	console.log("Called!")
+	// const webhookurl = answers[0] !== undefined && answers[0] !== "" ? answers[0] : "apple";
 	const webhookurl = answers[0]
+	const name = answers[1] !== undefined && answers[1] !== "" ? answers[1] : "EmbedSender";
+	const iconurl = answers[2] !== undefined && answers[2] !== "" ? answers[2] : "https://cdn.discordapp.com/attachments/1134772607615508530/1149685246363717652/hoshimatisuisei.gif";
 	const title = answers[3]
 	const desc = answers[4]
 	const color = answers[5]
@@ -13,8 +16,8 @@ function send(questions, answers) {
 	const webhookClient = new WebhookClient({ url: webhookurl });
 	console.log("Sending Webhook...")
 	webhookClient.send({
-		avatarURL: `https://cdn.discordapp.com/attachments/1134772607615508530/1149685246363717652/hoshimatisuisei.gif`,
-		username: `EmbedSender Test`,
+		avatarURL: iconurl,
+		username: name,
 		embeds: [{
 			title: title,
 			description: desc,
